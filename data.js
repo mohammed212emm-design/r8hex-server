9559e24c45afb4eab7df3e305826b7145cfd8cf9
mohammed ali <250900775+mohammed212emm-design@users.noreply.github.com>
2026-08-16 04:42:26 +0300

    Remove secret key from data.js

--- data.js
+++ data.js
@@ -1,6 +1,5 @@
 module.exports = {
   token: "8868693572:AAGP-mPAJe-pu2xaF4iX0oy84-bxBJHAdyE",
   id: "8000450636",
-  address: "https://r8hex-server-production.up.railway.app",
-  secret: "my-secret-key-123"
+  address: "https://r8hex-server-production.up.railway.app"
 };
